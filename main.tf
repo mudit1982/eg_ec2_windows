3773ee2e95eea6e19891339b9b86557c4c25520f
Mudit Pathak <mudit.pathak@coforge.com>
2023-11-24 19:08:46 +0530

    update

--- main.tf
+++ main.tf
@@ -29,7 +29,8 @@ data "aws_iam_policy_document" "default" {
   }
 }
 resource "aws_iam_role" "iam" {
-  name                 = local.iam_name
+  name                 = {local.iam_name}_IAM_Role
+  # name  = join (local.iam_name)
   path                 = "/"
   assume_role_policy   = data.aws_iam_policy_document.default.json
   #permissions_boundary = var.permissions_boundary_arn
