d054252ee0c57f3acc58bf6bfcd6dc797b978851
Mudit Pathak <mudit.pathak@coforge.com>
2023-12-22 13:17:38 +0530

    Remove existing SG rules

--- main.tf
+++ main.tf
@@ -60,10 +60,10 @@ module "new_security_group" {
 }
 
 
-module "existing_sg_rules" {
-  source = "./modules/existing_sg_rules"
-  existing_sg_rules = local.existing_sg_rules
-}
+# module "existing_sg_rules" {
+#   source = "./modules/existing_sg_rules"
+#   existing_sg_rules = local.existing_sg_rules
+# }
 
 
 
